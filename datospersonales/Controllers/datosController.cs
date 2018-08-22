8780f81dd23eea1fe9b90353b3136140d3166384
Centro de Desarrollo De Software <cds@univo.edu.sv>
2018-08-22 15:39:20 -0600

    subido

---
+++ datospersonales/Controllers/datosController.cs
@@ -0,0 +1,66 @@
+﻿using datospersonales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace datospersonales.Controllers
+{
+    public class datosController : Controller
+    {
+        datospersonalescontext obj=new datospersonalescontext();
+        // GET: datos
+        public ActionResult Index()
+        {
+            return View(obj.DatosPersonales.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(datospersonalesmodelo datos)
+        {
+            obj.DatosPersonales.Add(datos);
+            obj.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var datos = obj.DatosPersonales.Find(id);
+            return View(datos);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var datos = obj.DatosPersonales.Find(id);
+            return View(datos);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(datospersonalesmodelo datosPerson)
+        {
+            obj.Entry(datosPerson).State = System.Data.Entity.EntityState.Modified;
+            obj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var datos = obj.DatosPersonales.Find(id);
+            return View(datos);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(datospersonalesmodelo datos, int id)
+        {
+            var eliminar = obj.DatosPersonales.Find(id);
+            obj.DatosPersonales.Remove(eliminar);
+            obj.SaveChanges();
+            return View(); 
+        }
+    }
+}
---
+++ datospersonales/Models/datospersonalesmodelo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace datospersonales.Models
+{
+    public class datospersonalesmodelo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class datospersonalescontext : DbContext
+    {
+        public DbSet<datospersonalesmodelo> DatosPersonales { get; set; }
+
+
+
+    }
+}
